800d562a73b8b94d7bd2bb7ddf2c83fb5facc58e
franciscojs <franjs12@gmail.com>
2021-01-28 10:43:33 -0300

    Created Footer component.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="site-footer container">
+      Diseño y código por <a href="https://franciscojs.com">franciscojs</a> para
+      el challenge Alkemy Labs de enero 2021.
+    </footer>
+  );
+};
+
+export default Footer;
